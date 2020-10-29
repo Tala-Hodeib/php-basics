edb36e4a07a1462189970c8fec6f5610478a9c18
Tala-Hodeib <talahodeib@gmail.com>
2020-10-29 16:14:37 +0200

    Finished step4

---
+++ step4.php
@@ -0,0 +1,9 @@
+<?php
+$name = 'Tala';
+$position = 'student';
+$github_url = 'https://github.com/Tala-Hodeib';
+
+echo "Hello, I'm " .$name.", I'm a " .$position.'. Please check my github link ' .$github_url;
+echo "\nHello, I'm $name, I'm a $position. Please check my github link $github_url \n";
+echo 'Hello, I\'m '.$name.', I\'m a '.$position. '. Please check my github link ' .$github_url;
+?>
