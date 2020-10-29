28b98182c5e8050f5eeb13776870a6c092db5459
Tala-Hodeib <talahodeib@gmail.com>
2020-10-29 15:40:41 +0200

    Finished step3

---
+++ step3.php
@@ -0,0 +1,9 @@
+<?php
+$name = 'Tala';
+$email = 'talahodeib@gmail.com';
+echo "My name is $name and my email is $email." ;
+$height = '180';
+$width = '80';
+$area = $height * $width;
+echo $area;
+?>
