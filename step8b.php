d78cbf78c44c68201e8671f04e9f5036f9fd2b62
Tala-Hodeib <talahodeib@gmail.com>
2020-11-02 21:12:12 +0200

    Finished step8b

---
+++ step8b.php
@@ -0,0 +1,4 @@
+<?php
+$grocery2 = array( 'eggs'=>array( 'balade', 'mazere3' ), 'milk'=>array( 'Fresh', 'Taanayel' ), 'water-pack'=>array( 'Tanoureen', 'Reem' ) );
+echo 'Hey Sir, Please I need 1 pack of '.$grocery2['eggs'][0].' eggs and 3 '.$grocery2['water-pack'][1].' Water Pack';
+?>
